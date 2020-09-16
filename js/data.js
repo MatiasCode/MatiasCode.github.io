c0e847a72dbb54925bb6b7e0dc80f1670d51bc52
Matias Ricardo Mascareño <matias.ricardo.code@gmail.com>
2020-09-16 19:42:41 -0300

    no db

---
+++ js/data.js
@@ -0,0 +1,455 @@
+const data = [
+    {
+        "turno":"A",
+        "nombre": "Armellini (Av. Central y 31 Oeste)",
+        "lat": -33.3871774,
+        "lng": -60.1776638
+    },
+    {
+        "turno":"A",
+        "nombre": "Cabrera (Av. Falcon 222)",
+        "lat": -33.3366599,
+        "lng": -60.2129785
+    },
+    {
+        "turno":"A",
+        "nombre": "Diamante (Belgrano y Alvarez)",
+        "lat": -33.3444618,
+        "lng": -60.2280795
+    },
+    {
+        "turno":"A",
+        "nombre": "Furlan (9 de julio 260)",
+        "lat": -33.3336142,
+        "lng": -60.2296463
+    },
+    {
+        "turno":"A",
+        "nombre": "Gonzalez Pacin (Nacion 314)",
+        "lat": -33.3355785,
+        "lng": -60.2236905
+    },
+    {
+        "turno":"A",
+        "nombre": "Hector Lopez (Maipu 794)",
+        "lat": -33.3371376,
+        "lng": -60.2323137
+    },
+    {
+        "turno":"B",
+        "nombre": "Carrera (Almafuerte y España)",
+        "lat": -33.3388274,
+        "lng": -60.2134951
+    },
+    {
+        "turno":"B",
+        "nombre": "Coccaro (Rivadavia 987 bis)",
+        "lat": -33.3329622,
+        "lng": -60.2273605
+    },
+    {
+        "turno":"B",
+        "nombre": "Del Pueblo (Nacion 450)",
+        "lat": -33.3266697,
+        "lng": -60.2342123
+    },
+    {
+        "turno":"B",
+        "nombre": "Dotto (Pringles y Alvear)",
+        "lat": -33.3318645,
+        "lng": -60.2389851
+    },
+    {
+        "turno":"B",
+        "nombre": "Melone (Pte. Peron 858)",
+        "lat": -33.3306856,
+        "lng": -60.247207
+    },
+    {
+        "turno":"B",
+        "nombre": "Taljame (Alberdi 346)",
+        "lat": -33.3235471,
+        "lng": -60.2300551
+    },
+    {
+        "turno":"C",
+        "nombre": "Amefarma SCS (Mitre 200)",
+        "lat": -33.3237468,
+        "lng": -60.2321459
+    },
+    {
+        "turno":"C",
+        "nombre": "Garetto (Morteo y España)",
+        "lat": -33.3312479,
+        "lng": -60.2429275
+    },
+    {
+        "turno":"C",
+        "nombre": "Liliana Latorre (Ameghino 347)",
+        "lat": -33.336489,
+        "lng": -60.2191157
+    },
+    {
+        "turno":"C",
+        "nombre": "Ponce (Juramento 1445)",
+        "lat": -33.342801,
+        "lng": -60.2279518
+    },
+    {
+        "turno":"C",
+        "nombre": "Rasetto (Viale 401)",
+        "lat": -33.3227495,
+        "lng": -60.236997
+    },
+    {
+        "turno":"C",
+        "nombre": "San Nicolás (Cochabamba 357)",
+        "lat": -33.3310752,
+        "lng": -60.2310511
+    },
+    {
+        "turno":"D",
+        "nombre": "Boffa (Av. Savio 1142)",
+        "lat": -33.3592461,
+        "lng": -60.200964
+    },
+    {
+        "turno":"D",
+        "nombre": "Cej (Maipu 495)",
+        "lat": -33.3533087,
+        "lng": -60.2148297
+    },
+    {
+        "turno":"D",
+        "nombre": "De Los Arroyos (Nacion 102)",
+        "lat": -33.3464078,
+        "lng": -60.2186529
+    },
+    {
+        "turno":"D",
+        "nombre": "Lombardi (Alberdi 548)",
+        "lat": -33.3340065,
+        "lng": -60.2321578
+    },
+    {
+        "turno":"D",
+        "nombre": "Porta (Av. Savio 147)",
+        "lat": -33.350201,
+        "lng": -60.2233337
+    },
+    {
+        "turno":"D",
+        "nombre": "Romero (Pte. Peron 1648)",
+        "lat": -33.3642032,
+        "lng": -60.2556918
+    },
+    {
+        "turno":"E",
+        "nombre": "Almada (Maipu y J. B. Justo)",
+        "lat": -33.3513818,
+        "lng": -60.2191671
+    },
+    {
+        "turno":"E",
+        "nombre": "Catalan (Almafuerte 442)",
+        "lat": -33.3505637,
+        "lng": -60.2161852
+    },
+    {
+        "turno":"E",
+        "nombre": "De La Torre (Av. A. Illia 643)",
+        "lat": -33.3176436,
+        "lng": -60.2335905
+    },
+    {
+        "turno":"E",
+        "nombre": "Girardi (Av. Savio 1634)",
+        "lat": -33.3624522,
+        "lng": -60.1893758
+    },
+    {
+        "turno":"E",
+        "nombre": "Henrich (9 de Julio 63)",
+        "lat": -33.3477973,
+        "lng": -60.2221308
+    },
+    {
+        "turno":"E",
+        "nombre": "Tonon (Garibaldi 692)",
+        "lat": -33.3526134,
+        "lng": -60.2233337
+    },
+    {
+        "turno":"F",
+        "nombre": "Cantondebat (Brown 598)",
+        "lat": -33.3509076,
+        "lng": -60.2153984
+    },
+    {
+        "turno":"F",
+        "nombre": "Cavara (Italia y Necochea)",
+        "lat": -33.336011,
+        "lng": -60.2105618
+    },
+    {
+        "turno":"F",
+        "nombre": "Fenix (Garibaldi 281)",
+        "lat": -33.334397,
+        "lng": -60.2189803
+    },
+    {
+        "turno":"F",
+        "nombre": "Frattini (Moreno 108)",
+        "lat": -33.3477379,
+        "lng": -60.2432535
+    },
+    {
+        "turno":"F",
+        "nombre": "Prat (Pte. Peron 1093)",
+        "lat": -33.3516786,
+        "lng": -60.2469483
+    },
+    {
+        "turno":"F",
+        "nombre": "Zonta (Urquiza 422)",
+        "lat": -33.3244974,
+        "lng": -60.2296827
+    },
+    {
+        "turno":"G",
+        "nombre": "Barbotti (Bolivar 351)",
+        "lat": -33.3390941,
+        "lng": -60.2134951
+    },
+    {
+        "turno":"G",
+        "nombre": " Brasesco (Av. Savio y Pombo)",
+        "lat": -33.3602952,
+        "lng": -60.1940232
+    },
+    {
+        "turno":"G",
+        "nombre": " Cesari (Nacion 183)",
+        "lat": -33.3473367,
+        "lng": -60.2192951
+    },
+    {
+        "turno":"G",
+        "nombre": "Conde (Nacion 701)",
+        "lat": -33.3508228,
+        "lng": -60.2251938
+    },
+    {
+        "turno":"G",
+        "nombre": "Garaguso (Belgrano 320)",
+        "lat": -33.3475795,
+        "lng": -60.2238144
+    },
+    {
+        "turno":"G",
+        "nombre": "Prina (Av. Arturo Ilia 739)",
+        "lat": -33.3151132,
+        "lng": -60.2336927
+    },
+    {
+        "turno":"H",
+        "nombre": "Blanco (Almafuerte y Benitez) ",
+        "lat": -33.3531713,
+        "lng": -60.211823
+    },
+    {
+        "turno":"H",
+        "nombre": "Correa (Italia 38) ",
+        "lat": -33.3475696,
+        "lng": -60.2192951
+    },
+    {
+        "turno":"H",
+        "nombre": "Donatelli (Urquiza 499) ",
+        "lat": -33.3436723,
+        "lng": -60.2451574
+    },
+    {
+        "turno":"H",
+        "nombre": "Gagliardo (Pte. Peron 1035) ",
+        "lat": -33.3516786,
+        "lng": -60.2459375
+    },
+    {
+        "turno":"H",
+        "nombre": "Salvador (Moreno 220) ",
+        "lat": -33.3326546,
+        "lng": -60.2275111
+    },
+    {
+        "turno":"H",
+        "nombre": " Tioni (Rademil y Alvear)",
+        "lat": -33.3611786,
+        "lng": -60.1977818
+    },
+    {
+        "turno":"H",
+        "nombre": "Troffe (Av. Savio 238) ",
+        "lat": -33.3529776,
+        "lng": -60.2188704
+    },
+    {
+        "turno":"I",
+        "nombre": "Alonso (Don Bosco y Pellegrini) ",
+        "lat": -33.3359915,
+        "lng": -60.2223793
+    },
+    {
+        "turno":"I",
+        "nombre": "Ciminari (Alberdi 699) ",
+        "lat": -33.347326,
+        "lng": -60.244442
+    },
+    {
+        "turno":"I",
+        "nombre": "Garcia (Belgrano 184) ",
+        "lat": -33.3465186,
+        "lng": -60.2201794
+    },
+    {
+        "turno":"I",
+        "nombre": "Gomez (Pte. Peron 1366) ",
+        "lat": -33.3642032,
+        "lng": -60.2556918
+    },
+    {
+        "turno":"I",
+        "nombre": "Leone ",
+        "lat": -33.3550185,
+        "lng": -60.200088
+    },
+    {
+        "turno":"I",
+        "nombre": "Pza. Sarmiento (España y Rivadavia) ",
+        "lat": -33.3325652,
+        "lng": -60.2132468
+    },
+    {
+        "turno":"I",
+        "nombre": "Tonello (Falcon 651) ",
+        "lat": -33.3450187,
+        "lng": -60.2216368
+    },
+    {
+        "turno":"J",
+        "nombre": "Alluchon (Olleros 55) ",
+        "lat": -33.3341379,
+        "lng": -60.222751
+    },
+    {
+        "turno":"J",
+        "nombre": "Bongiorno (Francia y Alberdi) ",
+        "lat": -33.3452688,
+        "lng": -33.3452688
+    },
+    {
+        "turno":"J",
+        "nombre": "Bracco (Av. Savio 373) ",
+        "lat": -33.352152,
+        "lng": -60.2192951
+    },
+    {
+        "turno":"J",
+        "nombre": "Floreani (Garibaldi y Alem) ",
+        "lat": -33.3471825,
+        "lng": -60.2186529
+    },
+    {
+        "turno":"J",
+        "nombre": "Pinasco (Alvear 95) ",
+        "lat": -33.3508228,
+        "lng": -60.2245289
+    },
+    {
+        "turno":"J",
+        "nombre": "Prado (M. Cernadas 110) ",
+        "lat": -33.3538906,
+        "lng": -60.2090475
+    },
+    {
+        "turno":"K",
+        "nombre": "Andrada (Av. Savio 601) ",
+        "lat": -33.3530637,
+        "lng": -60.2168364
+    },
+    {
+        "turno":"K",
+        "nombre": " Lallo (Balcarce 441)",
+        "lat": -33.347391,
+        "lng": -60.2183032
+    },
+    {
+        "turno":"K",
+        "nombre": "Maria E. Barbotti (Lavalle 618) ",
+        "lat": -33.3371673,
+        "lng": -60.2313943
+    },
+    {
+        "turno":"K",
+        "nombre": "Maria I. Lopez (L. Guruciaga 103) ",
+        "lat": -33.3260773,
+        "lng": -60.2225977
+    },
+    {
+        "turno":"K",
+        "nombre": "Martinelli (Av. Morteo 74) ",
+        "lat": -33.354751,
+        "lng": -60.2247367
+    },
+    {
+        "turno":"K",
+        "nombre": "Palau (Av. Central 2215) ",
+        "lat": -33.3716893,
+        "lng": -60.1913058
+    },
+    {
+        "turno":"K",
+        "nombre": "Radium (Nación 352) ",
+        "lat": -33.3493005,
+        "lng": -60.220919
+    },
+    {
+        
+        "turno":"L",
+        "nombre": "Baroni (Av. Irigoyen 1272 -Bº Avamba’e) ",
+        "lat": -33.375946,
+        "lng": -60.208701
+    },
+    {
+        "turno":"L",
+        "nombre": "Capra (Moreno 466) ",
+        "lat": -33.3462705,
+        "lng": -60.2453984
+    },
+    {
+        "turno":"L",
+        "nombre": "Hegouaburu (Mitre 601) ",
+        "lat": -33.3514066,
+        "lng": -60.2257666
+    },
+    {
+        "turno":"L",
+        "nombre": "Maccaroni (Av. Savio 717) ",
+        "lat": -33.3546378,
+        "lng": -60.215793
+    },
+    {
+        "turno":"L",
+        "nombre": "Menna (Rivadavia 501) ",
+        "lat": -33.3509666,
+        "lng": -60.2129951
+    },
+    {
+        "turno":"L",
+        "nombre": "Pucciarelli (Lavalle 215 bis) ",
+        "lat": -33.3467543,
+        "lng": -60.2238144
+    }
+]
+
+export default data;
--- js/turno.js
+++ js/turno.js
@@ -1,6 +1,58 @@
+import data from './data.js';
+$(document).ready(() => {
+    
+    var hoy = moment().format('yyyy-MM-DD');
+    //alert(hoy)
+    $('#filterDate').val(hoy);
+    var turnos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+    $('#seeDate').click( (e) => {        
+        var input = $('#filterDate').val();
+        //alert(input)
+        fillList(makeDiff(input));
+    });
+    
+    var fecha_turno_a = moment('2020-09-06').format('YYYY-M-D');
+    //var diff = moment().diff(fecha_turno_a, 'days');
+    
+    function makeDiff(date){
+        var diff
+        if(date == null){
+            diff = moment().diff(fecha_turno_a, 'days');
+        }else{
+            diff = moment(date).diff(fecha_turno_a, 'days');
+        }
+        return diff
+    }
+    
 
-var hoy = new Date();
-$('#date_turno').pickadate();
-//$('#date_turno').val(hoy);
-console.log('Fecha ------>')
-console.log(hoy)
+    function check(d) {
+        console.log(`variable D => ${d}`)
+        if (d < 12) {
+            return turnos[d];
+        } else if (d == 12) {
+            return turnos[0]
+            // representa el turno A y debe guardarse la fecha en la db
+        }
+        else {
+
+            //let x = Math.floor(d / 11);
+            let x = d - 12;
+            return check(x)
+        }
+    }
+
+    function fillList(dt){
+        var turno_actual = check(dt);
+        //$('#contenList').child().remove();
+        $('#contentList').empty();
+        data.forEach(element => {
+            if(element.turno == turno_actual){
+                $('#contentList').append(`<li> <strong> ${element.nombre} </strong> </li>`);
+            }
+        });
+    }
+    
+
+    fillList(makeDiff(hoy));
+
+})
